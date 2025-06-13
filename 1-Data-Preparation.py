e707377c6338227334a3b0123d20026f8c32c0c3
fpaul787@gmail.com <fpaul787@gmail.com>
2025-06-13 20:09:56 +0000

    load our data

--- 1-Data-Preparation.py
+++ 1-Data-Preparation.py
@@ -1,2 +1,10 @@
 # Databricks notebook source
+# Read in data
+catalog = "frantzpaul_tech"
+schema = "wnba_rag"
+df = spark.read.table(f"{catalog}.{schema}.team_box_2025")
+display(df)
+
+# COMMAND ----------
+
 
