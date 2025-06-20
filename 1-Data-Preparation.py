c14599a2bd9469b36752500a3bc6cf91a2dc30ea
fpaul787@gmail.com <fpaul787@gmail.com>
2025-06-20 01:46:14 +0000

    Appending player and team box scores, saving the table

--- 1-Data-Preparation.py
+++ 1-Data-Preparation.py
@@ -6,52 +6,17 @@
 
 catalog = "frantzpaul_tech"
 schema = "wnba_rag"
-start_year = "2003"
-end_year = "2025"
-team_table_name_prefix = f"{catalog}.{schema}.team_box"
 team_box_combined_table_name = f"{catalog}.{schema}.team_box_combined"
-player_table_name_prefix = f"{catalog}.{schema}.player_box"
 player_box_combined_table_name = f"{catalog}.{schema}.player_box_combined"
 
 # COMMAND ----------
 
-def combine_stats_df(start_year, end_year, table_name_prefix, df):
-    combined_df = df
-    for year in range(int(start_year), int(end_year) + 1):
-        table_name = f"{table_name_prefix}_{year}"
-        df = spark.read.table(table_name)
-        
-        combined_df = combined_df.union(df)
-
-    return combined_df
-
+combined_team_box_df = spark.read.table(team_box_combined_table_name)
+combined_player_box_df = spark.read.table(player_box_combined_table_name)
 
 # COMMAND ----------
-
-table_name_2025 = "team_box_2025"
-team_box_schema = spark.table(f"{catalog}.{schema}.{table_name_2025}").schema
-
-team_box_df = spark.createDataFrame([], team_box_schema)
-team_box_df.write.mode("overwrite").saveAsTable(team_box_combined_table_name)
-
-
-player_name_2025 = "player_box_2025"
-player_box_schema = spark.table(f"{catalog}.{schema}.{player_name_2025}").schema
-
-player_box_df = spark.createDataFrame([], player_box_schema)
-player_box_df.write.mode("overwrite").saveAsTable(player_box_combined_table_name)
-
-# COMMAND ----------
-
-combined_team_box_df = combine_stats_df(start_year, end_year, team_table_name_prefix, team_box_df)
-combined_player_box_df = combine_stats_df(start_year, end_year, player_table_name_prefix, player_box_df)
-
-# Save to table
-combined_team_box_df.write.mode("overwrite").saveAsTable(team_box_combined_table_name)
-combined_player_box_df.write.mode("overwrite").saveAsTable(player_box_combined_table_name)
 
 display(combined_team_box_df)
-display(combined_player_box_df)
 
 # COMMAND ----------
 
@@ -63,9 +28,12 @@ display(wnba_team_box_clean_df)
 
 # COMMAND ----------
 
-from pyspark.sql import functions as F
+display(combined_player_box_df)
 
-wnba_team_box_clean_df = wnba_team_box_clean_df.filter(F.col("game_id") != "game_id")
+# COMMAND ----------
+
+wnba_player_box_clean_df = combined_player_box_df.drop("season_type", "athlete_id", "team_id", "team_short_display_name", "athlete_short_name", "athlete_position_abbreviation", "team_display_name", "team_uid", "team_slug", "team_logo", "team_abbreviation", "team_color", "team_alternate_color",  "opponent_team_id", "opponent_team_abbreviation", "opponent_team_logo", "opponent_team_color", "opponent_team_alternate_color", "athlete_headshot_href")
+display(wnba_player_box_clean_df)
 
 # COMMAND ----------
 
@@ -74,11 +42,25 @@ wnba_team_box_clean_df = wnba_team_box_clean_df.filter(F.col("game_id") != "game
 
 # COMMAND ----------
 
+wnba_player_box_clean_df.columns
+
+# COMMAND ----------
+
+"On {game_date} ({season}), {athlete_display_name} (#{{athlete_jersey}}, {athlete_position_name}) played for the {team_location} {team_name} in a {home_away} game against the {opponent_team_location} {opponent_team_name} ({opponent_team_display_name}). The final score was {team_score}-{opponent_team_score}, and the {'win' if team_winner else 'loss'} impacted {athlete_display_name}. He played {minutes} minutes, recording {points} points, shooting {field_goals_made}/{field_goals_attempted} from the field, {three_point_field_goals_made}/{three_point_field_goals_attempted} from three, and {free_throws_made}/{free_throws_attempted} from the line. He added {rebounds} rebounds ({offensive_rebounds} offensive, {defensive_rebounds} defensive), {assists} assists, {steals} steals, and {blocks} blocks, while committing {fouls} fouls and {turnovers} turnovers, finishing with a plus/minus of {plus_minus}. {'He was ejected.' if ejected else ''} {'Did not play due to ' + reason if did_not_play else ''}"
+
+# COMMAND ----------
+
 from pyspark.sql.functions import udf, struct
 from pyspark.sql.types import StringType
 
 @udf(returnType=StringType())
 def create_game_summary(row):
+    largest_lead_part = ""
+    if row.largest_lead is not None:
+        largest_lead_part = f"Their largest lead was {row.largest_lead} points."
+    else:
+        largest_lead_part = ""
+
     return (f"On {row['game_date']}, during the {row['season']} WNBA season, the {row['team_display_name']} played an {row['team_home_away']} game against the {row['opponent_team_display_name']}. "
     f"The final score was {row['team_score']}-{row['opponent_team_score']}, with the {row['team_name']} {'winning' if row['team_winner'] == True else 'losing'} the game. "
     f"The team recorded {row['points_in_paint']} points in the paint, {row['fast_break_points']} fast break points, "
@@ -89,15 +71,140 @@ def create_game_summary(row):
     f"{row['three_point_field_goals_made']}/{row['three_point_field_goals_attempted']} from three point range for a {row['three_point_field_goal_pct']}% 3PT. "
     f"They committed {row['turnovers']} turnovers, leading to {row['turnover_points']} opponent points, {row['team_turnovers']} team turnovers, and had {row['fouls']} personal fouls, "
     f"{row['technical_fouls']} technical fouls (total {row['total_technical_fouls']}) and {row['flagrant_fouls']} flagrant fouls. "
-    f"Their largest lead was {row['largest_lead']} points."
+    f"{largest_lead_part}"
     )
 
-wnba_summaries_df = wnba_clean_df.withColumn("game_summary", create_game_summary(struct(*wnba_clean_df.columns)))
-display(wnba_summaries_df)
+@udf(returnType=StringType())
+def create_player_game_summary(row):
+    """Create player summary"""
+
+    if row is None:
+        return None
+    
+    win_loss_part = ""
+    if row["team_winner"] is None:
+        win_loss_part = ""
+    else:
+        win_loss_part = "win" if row["team_winner"] == True else "loss"
+
+    ejected_part = ""
+    if row.ejected is not None and row.ejected == True:
+        ejected_part = "She was ejected."
+
+    did_not_play_part = ""
+    if row.did_not_play is not None and row.did_not_play == True:
+        if row.reason is not None:
+            did_not_play_part = "Did not play due to " + row.reason
+        else:
+            did_not_play_part = "Did not play."
+
+    jersey_part = ""
+    if row.athlete_jersey is not None:
+        jersey_part = f"#{row.athlete_jersey}."
+    else:
+        jersey_part = ""
+
+    position_part = ""
+    if row.athlete_position_name is not None and row.athlete_position_name != "Not Available":
+        position_part = f"{row.athlete_position_name},"
+    else:
+        position_part = ""
+
+    jersery_position_part = f", {jersey_part} {position_part}"
+    shooting_part = ""
+    if row.field_goals_made is not None and row.field_goals_attempted is not None:
+        shooting_part = (
+            f"shooting {row.field_goals_made}/{row.field_goals_attempted} from the field, "
+            f"{row.three_point_field_goals_made}/{row.three_point_field_goals_attempted} from three, "
+            f"and {row.free_throws_made}/{row.free_throws_attempted} from the line. "
+        )
+    else:
+        shooting_part = " "
+
+    plus_minus_part = ""
+    if row.plus_minus is not None:
+        plus_minus_part = f", finishing with a plus/minus was {row.plus_minus}. "
+    else:
+        plus_minus_part = ""
+
+    if row.did_not_play is not None and row.did_not_play == True:
+        reason = ""
+        if row.reason is not None:
+            reason = f"Did not play due to {row.reason}."
+        else:
+            reason = "Did not play."
+        
+        return (
+             f"On {row.game_date} ({row.season}), {row.athlete_display_name} "
+             f"(#{jersey_part}, {position_part}) {reason}"
+        )
+
+    
+    return (
+        f"On {row.game_date} ({row.season}), {row.athlete_display_name} "
+        f"{jersery_position_part} played for the {row.team_location} {row.team_name} "
+        f"in a {row.home_away} game against the {row.opponent_team_location} {row.opponent_team_name} ({row.opponent_team_display_name}). "
+        f"The final score was {row.team_score}-{row.opponent_team_score}, and the {win_loss_part} impacted {row.athlete_display_name}. "
+        f"She played {row.minutes} minutes, recording {row.points} points, {shooting_part}"
+        f"She added {row.rebounds} rebounds ({row.offensive_rebounds} offensive, {row.defensive_rebounds} defensive), "
+        f"{row.assists} assists, {row.steals} steals, and {row.blocks} blocks, while committing {row.fouls} fouls and {row.turnovers} turnovers"
+        f"{plus_minus_part}.{ejected_part}{did_not_play_part}"
+    )
+
+
 
 # COMMAND ----------
 
-df = wnba_summaries_df.select("game_id", "game_summary")
+wnba_team_summaries_df = wnba_team_box_clean_df.withColumn("game_summary", create_game_summary(struct(*wnba_team_box_clean_df.columns)))
+display(wnba_team_summaries_df)
+
+# COMMAND ----------
+
+wnba_player_summaries_df = wnba_player_box_clean_df.withColumn("player_summary", create_player_game_summary(struct(*wnba_player_box_clean_df.columns)))
+display(wnba_player_summaries_df)
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Append the rows together based on `game_id` and concat summary
+
+# COMMAND ----------
+
+from pyspark.sql import functions as F
+
+merged_team_box_df = wnba_team_summaries_df.groupBy("game_id").agg(
+  F.concat_ws("\n\n", F.collect_list("game_summary")).alias("summary")
+)
+
+merged_player_box_df = wnba_player_summaries_df.groupBy("game_id").agg(
+  F.concat_ws("\n\n", F.collect_list("player_summary")).alias("summary")
+)
+
+games_box_df = merged_team_box_df.withColumnRenamed("summary", "game_summary")
+players_box_df = merged_player_box_df.withColumnRenamed("summary", "player_summary")
+
+
+
+
+# COMMAND ----------
+
+combined_box_df = (
+    games_box_df.join(players_box_df, on="game_id", how="inner")
+    .withColumn(
+        "summary",
+        F.concat_ws("\n\n",
+                    F.coalesce(F.col("game_summary"), F.lit("")),
+                    F.coalesce(F.col("player_summary"), F.lit(""))
+                   )
+    )
+)
+
+display(combined_box_df)
+
+# COMMAND ----------
+
+df = combined_box_df.select("game_id", "summary")
 
 # save to table
 table_name = "wnba_game_summaries"
